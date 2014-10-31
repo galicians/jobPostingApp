60923e88047755b4e5892d9f6d774fd876d1ae67
galicians <galicians@gmail.com>
2014-10-31 18:29:17 +0000

    angular + jade binding

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('views', __dirname)
 app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(request, response) {
 	response.render('index');
