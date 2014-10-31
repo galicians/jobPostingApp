d274861672647f4db5dd8423290d62f82bf70a28
galicians <galicians@gmail.com>
2014-10-31 19:31:48 +0000

    changes for heroku

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = express();
 
+var port = process.env.PORT || 5000;
+
+
 app.set('views', __dirname)
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
@@ -10,4 +13,4 @@ app.get('*', function(request, response) {
 	response.render('index');
 });
 
-app.listen(5000);
+app.listen(port);
