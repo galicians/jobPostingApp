8f35d6038ab427a0075a782b8d086070cd86483b
galicians <galicians@gmail.com>
2014-11-01 14:01:51 +0000

    more refactoring on tests

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -9,17 +9,23 @@ function resetJobs() {
 	})
 }
 
+var connectDB = Promise.promisify(mongoose.connect, mongoose)
+
+function findJobs(query) {
+	return Promise.cast(mongoose.model('Job').find(query).exec())
+}
+
 describe('get jobs', function(){
 	it("should never be empty since jobs are seeded", function(done) {
-		mongoose.connect('mongodb://localhost/jobfinder', function() {
-			resetJobs()
+		connectDB('mongodb://localhost/jobfinder')
+			.then(resetJobs)
 			.then(jobModel.seedJobs)
-			.then(function() {	
-				mongoose.model('Job').find({}).exec(function(error, jobsList) {
-					expect(jobsList.length).to.be.at.least(1);
-					done();
-				});
+			.then(findJobs)
+			.then(function(jobsList) {
+				expect(jobsList.length).to.be.at.least(1);
+				done();
 			});
-		});	
-	});
-});	
+		});
+	});	
+
+
